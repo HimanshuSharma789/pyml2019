e764a0f6ad2a91acc1e4c233ed7d7d60b919f75f
HimanshuSharma789 <samitihs4@gmail.com>
2019-01-09 14:11:45 +0530

    Basic operation for beginning ml with python

--- first_pandas.py
+++ first_pandas.py
@@ -2,5 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 book = pd.read_csv("weather_report.csv")
 print(book)
-book.plot()
-
+plt.plot(book['date'], book['min'], color='r')
+plt.plot(book['date'], book['max'], color='b')
+plt.legend()
+plt.show()
