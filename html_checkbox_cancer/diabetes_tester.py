12528939158ace8bb9964559c386aefbb5efb292
HimanshuSharma789 <samitihs4@gmail.com>
2019-01-14 14:44:48 +0530

    diabetes and breast cancer detection using html and python -- sklearn -- webpy

--- html_checkbox_cancer/diabetes_tester.py
+++ html_checkbox_cancer/diabetes_tester.py
@@ -2,25 +2,39 @@ import ml_script
 import web
 
 urls = (
-    '/', 'Home'
+    '/', 'Home',
+    '/cancer', 'Cancer'
 )
 
 app = web.application(urls, globals())
 render = web.template.render('')
 
+results = []
 
 class Home:
     def GET(self):
-        return render.gui_home()
+        return render.diabetes()
 
     def POST(self):
         data = web.input()
-        seq_data = (data.preg, data.plas, data.pres, data.skin, data.test, data.mass, data.pedi, data.age)
-        result = ml_script.ml_script().result(seq_data)
-        if result:
-            return "Positive"
-        else:
-            return "Negative"
+        seq_data = (float(data.preg), float(data.plas), float(data.pres), float(data.skin),
+                    float(data.test), float(data.mass), float(data.pedi), float(data.age))
+        result = ml_script.ml_script("diabetes").result(seq_data)
+        results.append(result)
+        raise web.seeother('/cancer')
+
+
+class Cancer:
+    def GET(self):
+        return render.cancer()
+
+    def POST(self):
+        data = web.input()
+        seq_data = (float(data.rad), float(data.tex), float(data.per), float(data.area), float(data.smo),
+                    float(data.com), float(data.conc), float(data.conpt), float(data.sym), float(data.frac))
+        result = ml_script.ml_script("cancer").result(seq_data)
+        results.append(result)
+        return "Diabetes: ", float(results[0]), " \nBreast Cancer: ", float(results[1])
 
 
 if __name__ == "__main__":
--- html_checkbox_cancer/ml_script.py
+++ html_checkbox_cancer/ml_script.py
@@ -1,29 +1,41 @@
 from sklearn import tree
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsClassifier
 
-import numpy as np
+# import numpy as np
 import pandas as pd
 
 
 class ml_script:
-    def __init__(self):
-        self.file = pd.read_csv("pima-indians-diabetes-deepnet.csv")
-        # self.f = open("pima-indians-diabetes-deepnet.csv")
-        # self.f.readline()  # skip the header
-        # self.data = np.loadtxt(self.f, delimiter=",")
-        label = self.file['diabetes']
+    def __init__(self, disease):
+        if disease == "diabetes":
+            self.file = pd.read_csv("pima-indians-diabetes-deepnet.csv")
+        else:
+            self.file = pd.read_csv("breast-cancer-wisconsin-data.csv")
+
+        if disease == "diabetes":
+            label = self.file['diabetes']
+        else:
+            label = self.file['breast_cancer']
+
         features = self.file[self.file.columns[:-1]]
 
         self.dcs_algo = tree.DecisionTreeClassifier()
-        self.rf_algo = RandomForestRegressor(n_estimators=20, random_state=0)
+        self.knn_algo = KNeighborsClassifier(n_neighbors=5)
+        # self.rf_algo = RandomForestRegressor(n_estimators=20, random_state=0)
 
-        self.trained_rf = self.rf_algo.fit(features, label)
+        # self.trained_rf = self.rf_algo.fit(features, label)
+        self.trained_knn = self.knn_algo.fit(features, label)
         self.trained_dcs = self.dcs_algo.fit(features, label)
 
+        # self.f = open("pima-indians-diabetes-deepnet.csv")
+        # self.f.readline()  # skip the header
+        # self.data = np.loadtxt(self.f, delimiter=",")
+        #
         # self.trained_rf = self.rf_algo.fit(self.data[:, :-1], self.data[:, -1])
         # self.trained_dcs = self.dcs_algo.fit(self.data[:, :-1], self.data[:, -1])
 
     def result(self, info):
         out_dcs = self.trained_dcs.predict([list(info)])
-        out_rf = self.trained_rf.predict([list(info)])
-        return (out_rf + out_dcs) / 2
+        out_knn = self.trained_knn.predict([list(info)])
+        return (out_knn + out_dcs) / 2
