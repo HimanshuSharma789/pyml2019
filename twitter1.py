89f496812bfc6709d7c27101013eca3f99608822
HimanshuSharma789 <samitihs4@gmail.com>
2019-01-18 00:16:39 +0530

    using twitter tweepy api to access tweets

--- twitter1.py
+++ twitter1.py
@@ -1,26 +1,23 @@
-#pylint:disable=E0001
-import tweepy 
-  
+import tweepy
+
 # Fill the X's with the credentials obtained by  
 # following the above mentioned procedure. 
-consumer_key = "bSh5wvnGDnoE0HShQjbLcDCj5" 
+consumer_key = "bSh5wvnGDnoE0HShQjbLcDCj5"
 consumer_secret = "mwyr7FEk1z54uqhqxRSnHCoEM9QgdNVfEPNofH6gU3VwIbgfcs"
 access_key = "2953318536-Pm32gN5okawo4lcZlzpIIxfzvRpAuPWIZcbP9lT"
 access_secret = "BYzfYILbGoVrCfPt2exw4JBLnDBP8eKWDETOftiO285Ff"
-  
-# Function to extract 9
-          
-        # Authorization to consumer key and consumer secret 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
-  
-        # Access to user's access key and access secret 
-auth.set_access_token(access_key, access_secret) 
-  
-        # Calling api 
-api = tweepy.API(auth) 
+
+# Authorization to consumer key and consumer secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+# Access to user's access key and access secret
+auth.set_access_token(access_key, access_secret)
+
+# Calling api
+api = tweepy.API(auth)
 user = input()
-search = tweepy.Cursor(api.search, q= user, lang="en").items(5)
+search = tweepy.Cursor(api.search, q=user, lang="en").items(5)
 for item in search:
-	print("Username: " +item.user.name)
-	print(item.text)
-	print("\n")
+    print("Username: " + item.user.name)
+    print(item.text)
+    print("\n")
